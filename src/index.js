3715f0904cb8668570905d79ce519dd518aa89e0
Keid Achird <keidachird@gmail.com>
2023-04-16 15:08:57 +0300

    Add input validation

--- src/index.js
+++ src/index.js
@@ -42,8 +42,16 @@ const renderQueueNodes = queue => {
 // Event handlers
 const onEnqueue = () => {
   const input = queueInputEl.value
-  if (input === '' || queue.length >= MAX_NODES) return
+
+  if (input === '' || queue.length >= MAX_NODES) {
+    enqueueBtn.disabled = true
+    return
+  }
+
   queueInputEl.value = ''
+  queueInputEl.focus()
+  enqueueBtn.disabled = true
+  dequeueBtn.disabled = false
 
   queue.push(input)
   addQueueNode(input)
@@ -55,15 +63,29 @@ const onDequeue = () => {
     setQueueToLocalStorage(queue)
     removeQueueNode()
   }
+
+  dequeueBtn.disabled = !queue.length
+  /*
+    This fixes the bug when you have full queue,
+    after entering value in input and removing element from queue
+    the 'Add' button still remains disabled despite there is some text in input
+  */
+  enqueueBtn.disabled = queueInputEl.value === ''
 }
 
 // Event listeners
 queueInputEl.addEventListener('keypress', e => {
   if (e.key === 'Enter') onEnqueue()
 })
+queueInputEl.addEventListener('input', e => {
+  enqueueBtn.disabled = e.target.value === '' || queue.length >= MAX_NODES
+})
 enqueueBtn.addEventListener('click', onEnqueue)
 dequeueBtn.addEventListener('click', onDequeue)
 
 // Rendering queue
 const queue = getQueueFromLocalStorage()
+enqueueBtn.disabled = true
+dequeueBtn.disabled = !queue.length
+
 renderQueueNodes(queue)
